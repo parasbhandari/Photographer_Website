5a753bcb8bb70f8d31a0c9955c31bef49063f3ba
Paras Bhandari <parasbhandari2003@gmail.com>
2025-03-30 12:00:13 +0545

    fix wedding page issue

--- src/_js/header.js
+++ src/_js/header.js
@@ -13,8 +13,7 @@ class HeaderComponent extends HTMLElement {
         @import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css");
       </style>
       <link href="../_css/output.css" rel="stylesheet">
-      
-      <header class="bg-white text-black py-4 px-6 fixed w-full z-50 shadow-md" style="position: static">
+      <header class="bg-white text-black py-4 px-6 fixed w-full z-50 shadow-md">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
         <div class="max-w-7xl mx-auto flex justify-between items-center">
           <!-- Logo -->
